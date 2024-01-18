83eaeec5e845b2539966fca8a831b6e3262441fd
Coleman-Hedge <144719677+Coleman-Hedge@users.noreply.github.com>
2024-01-18 10:48:22 -0800

    Tests are now working

--- src/features/photos/photos.slice.js
+++ src/features/photos/photos.slice.js
@@ -14,7 +14,8 @@ const options = {
     // Task 1 Hint: You can use state.photos.unshift()
     // `unshift()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
     addPhoto: (state, action) => {
-      state.photos.unshift({id: state.photos.length + 1, photo: action.payload });
+      console.log("adding photo", action.payload)
+      state.photos.unshift({id: state.photos.length + 1, caption: action.payload.caption, imageUrl: action.payload.imageUrl });
     },
 
     // Task 6: Create an `removePhoto()` case reducer that removes a photo from state.photos
@@ -37,6 +38,11 @@ export default photosSlice.reducer;
 
 export const selectAllPhotos = (state) => state.photos.photos;
 export const selectFilteredPhotos = (state) => {
-  // Task 12: Complete `selectFilteredPhotos()` selector to return a filtered list of photos whose captions match the user's search term
-  return state.photos.photos.filter((photo) => photo.caption === selectSearchTerm(state));
+  const searchTerm = selectSearchTerm(state);
+  if (searchTerm.trim() === "") {
+    return state.photos.photos; // Return all photos if the search term is empty
+  } else {
+    console.log("returning filtered list for ", searchTerm);
+    return state.photos.photos.filter((photo) => photo.caption.toLowerCase().includes(searchTerm));
+  }
 };
